4742e3572c98b8f0d6fc5088237b828e62f6daef
Leonid <lenya.m1998@gmail.com>
2020-07-22 19:28:24 +0300

    Callback_functions

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 "use strict";
 
-const str = 'test';
-const arr = [1, 2, 4];
+function first() {
+   //do smth
+   setTimeout(function () {
+      console.log(1);
+   }, 500);
+}
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+function second() {
+   console.log(2);
+}
 
-const fruit = "Some fruit";
-console.log(fruit.indexOf('fr'));
+first();
+second();
 
-const logg = 'Hello world';
-console.log(logg.slice(6, 11));
+function learnJS(lang, callback) {
+   console.log(`im learning ${lang}`);
+   callback();
+}
 
-console.log(logg.substring(6, 11));
+function done() {
+   console.log('i passed this lesson');
+}
 
-console.log(logg.substr(6, 5));
-
-const num = 12.2;
-console.log(Math.round(num));
-
-const test = '12.2px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
+learnJS('JavaScript', done);
