894206f02382f62f37b4f1bf603e98de4f5bce54
Leonid <lenya.m1998@gmail.com>
2020-07-23 15:05:10 +0300

    pass_through_link_or_value__spread_operator

--- js/script.js
+++ js/script.js
@@ -1,33 +1,94 @@
 "use strict";
 
-const arr = [1, 92, 52, 61, 8];
-arr.sort(comparNum);
-console.log(arr);
+let a = 5,
+   b = a;
 
-function comparNum(a, b) {
-   return (a - b);
+b = b + 5;
+
+// console.log(a);
+// console.log(b);
+
+const obj = {
+   a: 5,
+   b: 1
+};
+
+const copy = obj; // не копия, а ссылка
+copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copyObject(mainObj) {
+   let objCopy = {};
+
+   for (let key in mainObj) {
+      objCopy[key] = mainObj[key];
+   }
+
+   return objCopy;
 }
-// arr.forEach(function (item, i, arr) {
-//    console.log(`№${i}: ${item} внутри массива ${arr}`);
-// });
 
+const number = {
+   a: 2,
+   b: 5,
+   c: {
+      x: 7,
+      y: 4
+   }
+};
 
+const newNumber = copyObject(number);
 
-// arr.pop();
-// arr.push(10);
+newNumber.a = 10;
+newNumber.c.x = 10;
 
+// console.log(newNumber);
+// console.log(number);
 
-// console.log(arr);
+const add = {
+   d: 17,
+   e: 20
+};
 
-// for (let i = 0; i < arr.length; i++) {
-//    console.log(arr[i]);
-// }
+const clone = Object.assign({}, add);
 
-// for (let value of arr) {
-//    console.log(value);
-// }
+clone.d = 20;
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join("; "));
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'kk';
+
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['utube', 'vimeo', 'rutube'],
+   blogs = ['wordpress', 'livejournal', 'blogger'],
+   internet = [...video, ...blogs, 'vk', 'fb'];
+
+console.log(internet);
+
+function log(a, b, c) {
+   console.log(a);
+   console.log(b);
+   console.log(c);
+}
+
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a', 'b'];
+const newArrray = [...array];
+
+const q = {
+   one: 1,
+   two: 2
+};
+
+const newq = {
+   ...q
+};
