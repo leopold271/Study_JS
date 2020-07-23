9ef5e3febde12c0afe9a4001ac797d323f6006b3
Leonid <lenya.m1998@gmail.com>
2020-07-23 15:40:42 +0300

    OOP_basics

--- js/script.js
+++ js/script.js
@@ -1,94 +1,30 @@
 "use strict";
 
-let a = 5,
-   b = a;
+let str = 'some';
+let strObj = new String(str);
 
-b = b + 5;
+console.log(typeof (str));
+console.log(typeof (strObj));
 
-// console.log(a);
-// console.log(b);
+console.dir([1, 2, 3]);
 
-const obj = {
-   a: 5,
-   b: 1
-};
-
-const copy = obj; // не копия, а ссылка
-copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copyObject(mainObj) {
-   let objCopy = {};
-
-   for (let key in mainObj) {
-      objCopy[key] = mainObj[key];
-   }
-
-   return objCopy;
-}
-
-const number = {
-   a: 2,
-   b: 5,
-   c: {
-      x: 7,
-      y: 4
+const soldier = {
+   health: 400,
+   armor: 100,
+   sayHello: function () {
+      console.log('Hello!!!');
    }
 };
 
-const newNumber = copyObject(number);
+const john = Object.create(soldier);
 
-newNumber.a = 10;
-newNumber.c.x = 10;
+// const john = {
+//    health: 100
+// };
 
-// console.log(newNumber);
-// console.log(number);
+//Object.setPrototypeOf(john, soldier);
 
-const add = {
-   d: 17,
-   e: 20
-};
+//john.__proto__ = soldier;
+//console.log(john);
 
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'kk';
-
-console.log(oldArray);
-console.log(newArray);
-
-const video = ['utube', 'vimeo', 'rutube'],
-   blogs = ['wordpress', 'livejournal', 'blogger'],
-   internet = [...video, ...blogs, 'vk', 'fb'];
-
-console.log(internet);
-
-function log(a, b, c) {
-   console.log(a);
-   console.log(b);
-   console.log(c);
-}
-
-const num = [2, 5, 7];
-log(...num);
-
-const array = ['a', 'b'];
-const newArrray = [...array];
-
-const q = {
-   one: 1,
-   two: 2
-};
-
-const newq = {
-   ...q
-};
+john.sayHello();
